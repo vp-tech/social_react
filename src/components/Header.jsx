6b2900990395209654a89acd2b2e7ba8207c8ead
VP <vladislavpodlipalin@gmail.com>
2020-12-03 14:03:29 +0600

    create project

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://img.favpng.com/20/11/13/logo-flower-royalty-free-png-favpng-T5kbUFNpSV0tFXRpGLzqXexZD.jpg"></img>
+    </header>
+}
+
+export default  Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Profile = () =>{
+    return <div className='content'>
+        <div>
+            <img src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg' />
+        </div>
+        <div>
+            ava + disc
+        </div>
+        <div>
+            New posts
+            <div>
+                My posts
+            </div>
+        </div>
+        <div>
+            <div>
+                1 posts
+            </div>
+            <div>
+                2 posts
+            </div>
+        </div>
+    </div>
+}
+export default Profile
