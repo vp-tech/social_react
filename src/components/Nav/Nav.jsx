4942a9985107cffa74b96a9f42c41b315813e218
vp-tech <vladislavpodlipalin@mail.ru>
2020-12-06 20:54:30 +0600

    switched to Navlink and changing css

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import s from './Nav.module.css'
+import {NavLink} from "react-router-dom";
 
-const Nav = () =>{
-    return  <nav className={s.nav}>
-        <div className={`${s.item} ${s.active}`}>
-            <a href='/profile'>Profile</a>
-        </div>
-        <div className={s.item}>
-            <a href='/dialogs'>Message</a>
-        </div>
-        <div className={s.item}>
-            <a href='/news'>News</a>
-        </div>
-        <div className={s.item}>
-            <a href='/music'>Music</a>
-        </div>
-        <div className={s.item}>
-            <a href='settings'>Settings</a>
-        </div>
-    </nav>
+const Nav = () => {
+    return (
+        <nav className={s.nav}>
+            <div className={s.item} >
+                <NavLink to='/profile' activeClassName={s.activeLink}>Profile</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/dialogs' activeClassName={s.activeLink}>Message</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/news' activeClassName={s.activeLink}>News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/music' activeClassName={s.activeLink}>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/settings' activeClassName={s.activeLink}>Settings</NavLink>
+            </div>
+        </nav>
+    )
 }
 export default Nav
